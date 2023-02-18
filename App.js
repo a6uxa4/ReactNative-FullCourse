156c292d1ab3b8a02c55c19a0e89424ad3f92768
a6uxa4 <a6uxa4@icloud.com>
2023-02-19 03:56:24 +0600

    save

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import TodoScreen from './src/screens/TodoScreen'
 
 export default function App() {
 	const [todoId, setTodoId] = useState(null)
-	const [todos, setTodos] = useState([])
+	const [todos, setTodos] = useState([{ id: 1, title: 'Todo' }])
 
 	function addTodo(title) {
 		setTodos((prev) => [
